fe8177075add8cb80480f16682c634c08ae7e8a1
jacks14235 <jack@stanley5.com>
2023-04-04 23:14:27 -0400

    work on fourier

--- fourier.py
+++ fourier.py
@@ -3,27 +3,77 @@ from scipy.io import wavfile # get the api
 import numpy as np
 import matplotlib.pyplot as plt
 
-BIT_DEPTH=16.
-CHUNK=1024
+freq_range = (80, 4000)
+# if __name__ == '__main__':
+	# BIT_DEPTH=16.
+	# CHUNK=1024
 
-fs, data = wavfile.read('stadium-rave.wav') # load the data
-a = data.T[0][CHUNK*100:CHUNK*101] # this is a two channel soundtrack, I get the first track
-print(len(a))
-print(a[:10])
-b=[(ele/2**BIT_DEPTH)*2-1 for ele in a] # normalize to [-1,1]
-c = fft(b) # calculate fourier transform (complex numbers list)
-d = len(c)/2  # you only need half of the fft list (real signal symmetry)
+	# fs, data = wavfile.read('loopback_record.wav') # load the data
+	# a = data.T[0][CHUNK*100:CHUNK*101] # this is a two channel soundtrack, I get the first track
+	# print(len(a))
+	# print(a[:10])
+	# b=[(ele/2**BIT_DEPTH)*2-1 for ele in a] # normalize to [-1,1]
+	# c = fft(b) # calculate fourier transform (complex numbers list)
+	# d = len(c)/2  # you only need half of the fft list (real signal symmetry)
+
+	# print(c)
 
 
+	# N = 128
+	# # # sample spacing
+	# T = 1.0 / 800.0
+	# # x = np.linspace(0.0, len(a), N, endpoint=False)
+	# # y = c
+	# yf = c
+	# xf = fftfreq(N, 44100)[:N//2]
 
-N = 12
-# sample spacing
-T = 1.0 / 800.0
-x = np.linspace(0.0, len(a), N, endpoint=False)
-y = c
-yf = fft(y)
-xf = fftfreq(N, T)[:N//2]
+	# plt.plot(xf[2:], 2.0/N * np.abs(yf[0:N//2])[2:])
+	# plt.grid()
+	# plt.show()
 
-plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
-plt.grid()
-plt.show()
+class TestReader:
+	def __init__(self):
+		self.CHUNK = 1024
+		self.BUCKETS = 64
+		self.DEPTH = 16
+		self.SAMPLE = 44100
+
+		fs, data = wavfile.read('loopback_record.wav') # load the data
+		data = data[:, 0] / (2**(self.DEPTH - 1))
+		self.data = data # you only need half of the fft list (real signal symmetry)
+		print(data[4932:4940])
+		self.pointer = 0
+		print("Data max", max(data))
+
+	def _seek(self, n):
+		self.pointer = n
+    
+	def getNext(self):
+		if self.pointer + self.CHUNK > len(self.data):
+			return None
+		curr = self.data[self.pointer:self.pointer+self.CHUNK]
+		self.pointer += self.CHUNK
+		return curr
+	
+	def getNextVals(self, n_buckets=12):
+		curr = self.getNext()
+		curr = np.array(curr) / 2**self.DEPTH
+		N = self.CHUNK
+
+		yf = fft(curr)
+		xf = fftfreq(N, 1 / self.SAMPLE)
+
+		buckets = np.logspace(freq_range[0], freq_range[1], num=12, base=2)
+		
+
+		return xf, yf
+
+
+if __name__ == '__main__':
+	reader = TestReader()
+	reader._seek(((2*44100)//1024)*1024)
+
+	xf, yf = reader.getNextVals()
+	print(len(xf), len(yf))
+	plt.plot(xf[:500], np.abs(yf)[:500])
+	plt.show()
--- game_test.py
+++ game_test.py
@@ -1,17 +1,22 @@
 import pygame
 import numpy as np
 import math
+import fourier
+
+
+WIDTH = 400
+HEIGHT = 400
 
 pygame.init()
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 color = lambda x: 255 * ((math.sin(.005 * x) + 1) / 2)
 
 x = 0
-image = np.zeros((400, 400, 3), dtype=np.uint8)
+image = np.zeros((WIDTH, HEIGHT, 3), dtype=np.uint8)
 
-left = image[:200]
-right = image[200:]
+
+current_vals = [.16, .33, .5, .67, .83, 1, .83, .66,.5,.33,.17,0]
 
 running = True
 while running:
@@ -21,8 +26,17 @@ while running:
 
     # Update the image
     x += 1
-    
-    left[:,:] = np.array([255, 0, color(x)])
+
+    image = np.zeros((WIDTH, HEIGHT, 3))
+
+    bucket_width = WIDTH / len(current_vals)
+    for i, val in enumerate(current_vals):
+        height = int(HEIGHT * val)
+        start = int(i * bucket_width)
+        end = int((i+1) * bucket_width)
+        print(start, end, height)
+        image[start:end, :height, :] = np.array([255, 0, 0])
+
 
     # Create a new surface from the updated array
     surface = pygame.surfarray.make_surface(image)
